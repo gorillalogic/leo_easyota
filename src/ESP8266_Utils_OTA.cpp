aebc5ebc08892ac16baa2024c0c80b4105e79b47
Emmanuel Mora <emmanuel.mora@gorillalogic.com>
2020-06-17 22:18:51 -0600

    Add ArduinoLog and style fixes.

--- src/ESP8266_Utils_OTA.cpp
+++ src/ESP8266_Utils_OTA.cpp
@@ -3,54 +3,40 @@
 #include <WiFiUdp.h>
 #include "ESP8266_Utils_OTA.h"
 #include <ArduinoOTA.h>
+#include <ArduinoLog.h>
 
+const char * const TAG = "EasyOTA";
 
 void initOTA()
 {
 
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-
-  // Hostname defaults to esp8266-[ChipID]
-  // ArduinoOTA.setHostname(hostname);  // Defined in config.h
-
-  // No authentication by default
-  // ArduinoOTA.setPassword((const char *)"123");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
   ArduinoOTA.onStart([]() {
-    Serial.println("Start");
+    Log.notice("%s: onStart", TAG);
   });
-  
+
   ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
+    Log.notice("%s: onEnd invoked.", TAG);
   });
 
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    Log.notice("%s: Progress: %d", TAG, progress / (total / 100) );
   });
 
   ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
+    Log.error("%s: onError: %u", TAG, error);
     if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
+      Log.error("%s: onError OTA_AUTH_ERROR", TAG);
+    } else if (error == OTA_BEGIN_ERROR){
+      Log.error("%s: onError OTA_BEGIN_ERROR", TAG);
+    } else if (error == OTA_CONNECT_ERROR){
+      Log.error("%s: onError OTA_CONNECT_ERROR", TAG);
     } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
+      Log.error("%s: onError OTA_RECEIVE_ERROR", TAG);
+    } else if (error == OTA_AUTH_ERROR) {
+      Log.error("%s: onError OTA_AUTH_ERROR", TAG);
     }
   });
 
   ArduinoOTA.begin();
-
-  Serial.println("");
-  Serial.println("OTA started");
-
+  Log.notice("%s: OTA started.", TAG);
 }

43814f7e1e54f5e27e0b866cce624317eee736c3
Emmanuel Mora <emmanuel.mora@gorillalogic.com>
2020-06-21 19:31:21 -0600

    Add new line print.

--- src/EasyOTA.cpp
+++ src/EasyOTA.cpp
@@ -8,32 +8,32 @@ void initOTA()
 {
 
   ArduinoOTA.onStart([]() {
-    Log.notice("%s: onStart", TAG);
+    Log.notice("%s: onStart" CR, TAG);
   });
 
   ArduinoOTA.onEnd([]() {
-    Log.notice("%s: onEnd invoked.", TAG);
+    Log.notice("%s: onEnd invoked." CR, TAG);
   });
 
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Log.notice("%s: Progress: %d", TAG, progress / (total / 100) );
+    Log.notice("%s: Progress: %d" CR, TAG, progress / (total / 100) );
   });
 
   ArduinoOTA.onError([](ota_error_t error) {
-    Log.error("%s: onError: %u", TAG, error);
+    Log.error("%s: onError: %u" CR, TAG, error);
     if (error == OTA_AUTH_ERROR) {
-      Log.error("%s: onError OTA_AUTH_ERROR", TAG);
+      Log.error("%s: onError OTA_AUTH_ERROR" CR, TAG);
     } else if (error == OTA_BEGIN_ERROR){
-      Log.error("%s: onError OTA_BEGIN_ERROR", TAG);
+      Log.error("%s: onError OTA_BEGIN_ERROR" CR, TAG);
     } else if (error == OTA_CONNECT_ERROR){
-      Log.error("%s: onError OTA_CONNECT_ERROR", TAG);
+      Log.error("%s: onError OTA_CONNECT_ERROR" CR, TAG);
     } else if (error == OTA_RECEIVE_ERROR) {
-      Log.error("%s: onError OTA_RECEIVE_ERROR", TAG);
+      Log.error("%s: onError OTA_RECEIVE_ERROR" CR, TAG);
     } else if (error == OTA_AUTH_ERROR) {
-      Log.error("%s: onError OTA_AUTH_ERROR", TAG);
+      Log.error("%s: onError OTA_AUTH_ERROR" CR, TAG);
     }
   });
 
   ArduinoOTA.begin();
-  Log.notice("%s: OTA started.", TAG);
+  Log.notice("%s: OTA started." CR, TAG);
 }
